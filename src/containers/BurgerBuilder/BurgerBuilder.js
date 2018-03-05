caa9df699bfef4db0a35a3f47cb511b603e13927
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2018-03-06 00:21:48 +0100

    Create build control component and output multiple build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -16,8 +17,8 @@ class BurgerBuilder extends Component {
     render() {
         return (
             <Auxiliary>
-                <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </Auxiliary>
         );
     }
